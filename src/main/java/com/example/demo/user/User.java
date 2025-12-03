446e2d3212603a50579d7507981421fdb6320c71
lucatonik8@gmail.com <lucatonik8@gmail.com>
2025-12-03 13:26:24 +0100

    Fix del HelloController y el Pom

--- src/main/java/com/example/demo/user/User.java
+++ src/main/java/com/example/demo/user/User.java
@@ -1,13 +1,107 @@
 package com.example.demo.user;
 
-public record User(
-        Integer id,
-        String nombre,
-        String apellidos,
-        String email,
-        String telefono,
-        String dni,
-        String direccionPostal,
-        Boolean socio,
-        String password
-) {}
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private String apellidos;
+    private String email;
+    private String telefono;
+    private String dni;
+    private String direccionPostal;
+    private Boolean socio;
+    private String password;
+
+    public User() {
+    }
+
+    public User(String nombre, String apellidos, String email, String telefono, String dni, String direccionPostal, Boolean socio, String password) {
+        this.nombre = nombre;
+        this.apellidos = apellidos;
+        this.email = email;
+        this.telefono = telefono;
+        this.dni = dni;
+        this.direccionPostal = direccionPostal;
+        this.socio = socio;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getDireccionPostal() {
+        return direccionPostal;
+    }
+
+    public void setDireccionPostal(String direccionPostal) {
+        this.direccionPostal = direccionPostal;
+    }
+
+    public Boolean getSocio() {
+        return socio;
+    }
+
+    public void setSocio(Boolean socio) {
+        this.socio = socio;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
+
